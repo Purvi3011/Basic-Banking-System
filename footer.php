0b61a574adc67c664013b9ffb9287cdab307c3c2
Purvi3011 <101950229+Purvi3011@users.noreply.github.com>
2022-03-20 23:40:34 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #02001f;
+  color: white;
+  text-align: center;
+}
+</style>
+
+<div class="footer">
+<p><small>&copy 2022. Made by <b>Purvi Parmar</b><br>GRIP TheSparksFoundation.</small></p>
+</div>
